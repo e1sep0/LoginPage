5736089d8339d2104fbb788fdcc37ab1e91be82b
Frolov Anton <antonfrolov007@mail.ru>
2013-07-28 20:14:54 +0400

    Попытки залогиниться на truba.com

--- LoginMod/src/Login/LoginClass.java
+++ LoginMod/src/Login/LoginClass.java
@@ -1,54 +1,120 @@
 package Login;
 
+import org.testng.Assert;
+import org.openqa.selenium.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
-
-public class LoginClass  {
-    public static void main(String[] args) throws InterruptedException {
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
-        WebDriver driver = new ChromeDriver();
-
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
-
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
-
-        // Enter something to search for
-        element.sendKeys("Cheese!");
-        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
-
-           Thread.sleep(1000);
+import org.testng.annotations.*;
+import java.io.File;
+import jxl.*;
 
 
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
 
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("cheese!");
-            }
-        });
+public class LoginClass {
 
-        // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
 
-        //Close the browser
-        driver.quit();
+
+    private WebDriver driver;
+
+    private StringBuffer verificationErrors = new StringBuffer();
+
+
+    @BeforeTest
+    public void setUp() throws Exception {
+
+            driver = new FirefoxDriver();
+
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
+
+    @DataProvider(name = "DP1")
+    public Object[][] createData1() throws Exception{
+        Object[][] retObjArr=getTableArray("Test\\Data\\data1.xls",
+                "LogPass", "TestData");
+        return(retObjArr);
+    }
+
+    @Test(dataProvider = "DP1")
+    public void testUntitled(String Login, String Password) throws Exception {
+
+        driver.get("http://truba.com");
+        driver.findElement(By.className("enter")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Thread.sleep(2000);  - без этой задержки не работает
+
+        driver.switchTo().frame("");
+        Thread.sleep(3000);
+        driver.findElement(By.name("username")).sendKeys(Login);
+
+        driver.findElement(By.name("password")).sendKeys(Password);
+        driver.findElement(By.name("password")).submit();
+
+
+        if(!isElementPresent_1(driver,By.xpath(".//*[@id='error']"))){
+            System.out.println("Логин "+Login+" и пароль "+Password+" - верны!");
+            driver.findElement(By.className("exit")).click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        } else
+        {
+            System.out.println("Логин "+Login+" и пароль "+Password+" - неверны!");
+        }
+        }
+
+
+
+
+
+    @AfterTest
+    public void tearDown() throws Exception {
+
+        driver.quit();
+
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            Assert.fail(verificationErrorString);
+        }
+    }
+    public String[][] getTableArray(String xlFilePath, String sheetName, String tableName) throws Exception{
+        String[][] tabArray=null;
+
+        Workbook workbook = Workbook.getWorkbook(new File(xlFilePath));
+        Sheet sheet = workbook.getSheet(sheetName);
+        int startRow,startCol, endRow, endCol,ci,cj;
+        Cell tableStart=sheet.findCell(tableName);
+        startRow=tableStart.getRow();
+        startCol=tableStart.getColumn();
+
+        Cell tableEnd= sheet.findCell(tableName, startCol+1,startRow+1, 100, 64000,  false);
+
+        endRow=tableEnd.getRow();
+        endCol=tableEnd.getColumn();
+        System.out.println("startRow="+startRow+", endRow="+endRow+", " +
+                "startCol="+startCol+", endCol="+endCol);
+        tabArray=new String[endRow-startRow-1][endCol-startCol-1];
+        ci=0;
+
+        for (int i=startRow+1;i<endRow;i++,ci++){
+            cj=0;
+            for (int j=startCol+1;j<endCol;j++,cj++){
+                tabArray[ci][cj]=sheet.getCell(j,i).getContents();
+            }
+        }
+
+
+        return(tabArray);
+    }
+    public boolean isElementPresent_1(WebDriver driver, By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+
 }
